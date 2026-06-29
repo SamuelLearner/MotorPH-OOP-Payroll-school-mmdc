16cf0b6e60b2e5daca0aae72777e8d9bacc3791c
H3Samuel <lr.spocampo@mmdc.mcl.edu.ph>
2026-06-29 14:28:59 +0800

    oops

--- src/motorph/util/DatabaseConnection.java
+++ src/motorph/util/DatabaseConnection.java
@@ -14,7 +14,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/motorph_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Manila";
     private static final String USER = "root";
-    private static final String PASSWORD = "TR6ghTcxRy"; // <-- SET YOUR MYSQL PASSWORD HERE
+    private static final String PASSWORD = ""; // <-- SET YOUR MYSQL PASSWORD HERE
 
     private static Connection connection;
 
